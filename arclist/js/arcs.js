4528684a96391ed2d5525baf81523e2bdeb3deef
Chris Morrow <morrow181@gmail.com>
2020-06-13 13:47:42 -0400

    completed arc and list view with transitions / added navigation to switch views

--- arclist/js/arcs.js
+++ arclist/js/arcs.js
@@ -1,45 +1,109 @@
-// det data
-getJson(createVis, "data/top50.json");
+const visEl = document.getElementById("vis");
+let svgHeight = visEl.clientHeight;
+let svgWidth = visEl.clientWidth;
+let data, minSale, maxSale;
 
-d3.select("#views").selectAll("a").on("click", viewChange);
+// arc selections
+let svg, visPaths, mousePaths;
+// list selections
+const listView = d3.select("#listView");
+let list, item;
+
+const margin = 50,
+  x0 = margin,
+  pathYDown = 100,
+  downTime = 1000,
+  defaultTime = 2000,
+  overlayXOffset = 35,
+  overlayYOffset = 20;
+
+// updated dimensions
+let y0 = svgHeight,
+  x1 = svgWidth - margin * 2,
+  y = svgHeight,
+  xCenter = svgWidth / 2,
+  topY = -(svgHeight - margin);
+
+const el = d3.select("#vis").on("mousemove", positionOverlay);
+
+const overlay = el.select("#overlay");
+
+const yScale = d3.scaleLinear();
+const xScale = d3.scaleLinear();
+
+// det data
+getJson(init, "data/top50.json");
+const viewLinks = d3.select("#views").selectAll("a").on("click", viewChange);
+
+function init(json) {
+  data = transformData(JSON.parse(json));
+  // set scales
+  minSale = d3.min(data, (d) => {
+    const num = Number(d.sales_total);
+    return num;
+  });
+  maxSale = d3.max(data, (d) => {
+    const num = Number(d.sales_total);
+    return num;
+  });
+  // build arc vis
+  buildArcs();
+
+  window.onresize = updateArcs;
+}
 
 function viewChange() {
   d3.event.preventDefault;
-  console.log("view change");
-  console.log(this.getAttribute("rel"));
+  viewLinks.classed("active", false);
+  this.classList.toggle("active");
   if (this.getAttribute("rel") == "list") {
+    showListView();
+  } else {
+    showArcView();
   }
 }
 
-function createVis(json) {
-  const el = d3.select("#vis").on("mousemove", positionOverlay);
-  const visEl = document.getElementById("vis");
-  const svgHeight = visEl.clientHeight;
-  const svgWidth = visEl.clientWidth;
+async function showArcView() {
+  const item = listView.selectAll(".item");
+  svg.style("opacity", 0);
+  item
+    .select(".row")
+    .style("opacity", 1)
+    .transition()
+    .duration(defaultTime)
+    .style("opacity", 0);
 
-  const yScale = d3.scaleLinear();
-  const xScale = d3.scaleLinear();
-  const margin = 50,
-    x0 = margin,
-    y0 = svgHeight,
-    x1 = svgWidth - margin * 2,
-    y = svgHeight,
-    xCenter = svgWidth / 2,
-    pathYDown = 100,
-    topY = -(svgHeight - margin);
+  item.select(".bar").transition().duration(defaultTime).style("width", "0%");
 
-  const downTime = 1000,
-    defaultTime = 2000,
-    overlayXOffset = 35,
-    overlayYOffset = 20;
+  await item
+    .select(".bar-bg")
+    .style("opacity", 1)
+    .transition()
+    .duration(defaultTime)
+    .style("opacity", 0)
+    .end();
 
-  const data = transformData(JSON.parse(json));
+  listView.classed("none", true);
 
-  var svg = d3
+  updateArcs();
+}
+
+async function showListView() {
+  svg.transition().duration(1000).style("opacity", 0);
+  mousePaths.transition().duration(downTime).attr("d", pathOut);
+  await visPaths.transition().duration(downTime).attr("d", pathOut).end();
+  overlay.classed("none", true);
+  buildList(data);
+}
+
+function buildArcs() {
+  listView.classed("none", true);
+  overlay.classed("none", false);
+
+  svg = d3
     .select("div#vis")
     .append("svg")
     .attr("preserveAspectRatio", "xMinYMin meet")
-    .attr("viewBox", `0 0 ${svgWidth} ${svgHeight}`)
     .classed("svg-content", true);
 
   const svgDefs = svg.append("defs");
@@ -49,19 +113,9 @@ function createVis(json) {
   gradient.append("stop").attr("class", "stop-middle").attr("offset", "50%");
   gradient.append("stop").attr("class", "stop-right").attr("offset", "100%");
 
-  // set scales
-  const minSale = d3.min(data, (d) => {
-    const num = Number(d.sales_total);
-    return num;
-  });
-  const maxSale = d3.max(data, (d) => {
-    const num = Number(d.sales_total);
-    return num;
-  });
-
   // init lines straight on bottom
-  const overlay = el.select("#overlay");
-  const visPaths = svg
+
+  visPaths = svg
     .selectAll("path")
     .data(data)
     .enter()
@@ -70,7 +124,7 @@ function createVis(json) {
     .classed("gradient-stroke", true)
     .attr("d", createLine);
 
-  const mousePaths = svg
+  mousePaths = svg
     .selectAll("path.hidden")
     .data(data)
     .enter()
@@ -81,37 +135,47 @@ function createVis(json) {
     .classed("hidden", true)
     .attr("d", createLine);
 
+  updateArcs();
+}
+
+async function updateArcs() {
+  svg.style("opacity", 0).transition().duration(1000).style("opacity", 1);
+  (y0 = svgHeight),
+    (x1 = svgWidth - margin * 2),
+    (y = svgHeight),
+    (xCenter = svgWidth / 2),
+    (topY = -(svgHeight - margin));
+
+  svg.attr("viewBox", `0 0 ${svgWidth} ${svgHeight}`);
+
+  overlay.classed("none", false).classed("active", false);
+
   yScale.domain([maxSale, minSale]).range([topY, svgHeight]);
 
   // transition paths up from bottom
-  visPaths.data(data).transition().duration(defaultTime).attr("d", pathIn);
-  mousePaths.data(data).transition().duration(defaultTime).attr("d", pathIn);
+  mousePaths
+    .transition()
+    .duration(defaultTime)
+    .ease(d3.easeBack)
+    .attr("d", pathIn);
+  await visPaths
+    .transition()
+    .duration(defaultTime)
+    .ease(d3.easeBack)
+    .attr("d", pathIn);
+}
 
-  function update() {
-    // do stuff
-  }
-
-  function pathClick() {
-    showListView();
-  }
-
-  function showListView() {
-    visPaths.transition().duration(downTime).attr("d", pathOut);
-    mousePaths.transition().duration(downTime).attr("d", pathOut);
-    const listView = d3.select("#listView").classed("visible", true);
-    buildList(data);
-  }
-
-  function buildList(data) {
-    xScale.domain([minSale, maxSale]).range([0, 100]);
-    const list = d3.select("#listView .items").selectAll("div.item").data(data);
-    const item = list.enter().append("div").classed("item", true);
-
-    //   list.node().classList.add('item row')
-
-    // const items = list.selectAll("div.item");
-
-    const row = item.append("div").classed("row", true);
+function buildList(data) {
+  listView.classed("none", false);
+  let row, item;
+  let list = d3.select("#listView .items").selectAll("div.item");
+  xScale.domain([minSale, maxSale]).range([0, 100]);
+  listView.classed("none", false);
+  // enter
+  if (list.nodes().length === 0) {
+    list = d3.select("#listView .items").selectAll("div.item").data(data);
+    item = list.enter().append("div").classed("item", true);
+    row = item.append("div").classed("row", true);
 
     row
       .append("div")
@@ -125,113 +189,129 @@ function createVis(json) {
 
     item
       .append("div")
-      .classed("bar row", true)
+      .classed("bar-bg", true)
+      .append("div")
+      .classed("bar", true)
+      .style("width", "0")
+      .transition()
+      .duration(defaultTime)
       .style("width", (d) => `${xScale(d.sales_total)}%`);
   }
 
-  function positionOverlay(d, i) {
-    const mousePos = d3.mouse(this);
-    overlay.style("left", `${mousePos[0] + overlayXOffset}px`);
-    overlay.style("top", `${mousePos[1] - overlayYOffset}px`);
-  }
+  // update
+  item = d3.select("#listView").selectAll(".item");
+  row = item.select(".row");
 
-  function pathMouseOver(d, index) {
-    const pathData = d;
-    overlay.classed("active", true);
-    visPaths.each(function (d, i) {
-      if (i === index) {
-        d3.select(this).classed("hover", true).classed("muted", false);
-      } else {
-        d3.select(this).classed("hover", false);
-      }
-      overlay
-        .classed("active", true)
-        .select(".title")
-        .html(pathData.item_description);
+  row
+    .style("opacity", 0)
+    .transition()
+    .duration(defaultTime)
+    .style("opacity", 1);
 
-      const salesString = numToCurrency.format(pathData.sales_total);
+  item
+    .select(".bar-bg")
+    .style("opacity", 0)
+    .transition()
+    .duration(defaultTime)
+    .style("opacity", 1);
 
-      overlay.select(".number").html(salesString);
-    });
-  }
+  item
+    .select(".bar")
+    .style("width", "0")
+    .transition()
+    .duration(defaultTime)
+    .style("width", (d) => `${xScale(d.sales_total)}%`);
 
-  function pathMouseOut(d, index) {
-    visPaths.classed("hover", false);
-    overlay.classed("active", false);
-  }
+  // update
+}
+// end buildList
 
-  function createLine(d) {
-    //   const x1 = 200;
-    const y1 = 400;
-    const points = [
-      [x0, y0],
-      [xCenter, svgWidth],
-      [x1, svgHeight],
-    ];
-    const path = d3.path();
-    path.moveTo(x0, y0);
-    path.quadraticCurveTo(...points[1], ...points[2]);
-    return path;
-  }
-
-  function pathIn(d) {
-    const points = [
-      [x0, y0],
-      [xCenter, yScale(d.sales_total)],
-      [x1, svgHeight],
-    ];
-    const path = d3.path();
-    path.moveTo(x0, y0);
-    path.quadraticCurveTo(...points[1], ...points[2]);
-    return path;
-  }
-
-  function pathDown(d) {
-    const points = [
-      [x0, y0],
-      [xCenter, yScale(d.sales_total) + pathYDown],
-      [x1, svgHeight],
-    ];
-    const path = d3.path();
-    path.moveTo(x0, y0);
-    path.quadraticCurveTo(...points[1], ...points[2]);
-    return path;
-  }
-
-  function pathOut(d) {
-    // { cpx: xCenter, cpy: 400 },
-    const points = [
-      [x0, y0],
-      [xCenter, svgHeight],
-      [x1, svgHeight],
-    ];
-    const path = d3.path();
-    path.moveTo(x0, y0);
-    path.quadraticCurveTo(...points[1], ...points[2]);
-    return path;
-  }
+function pathClick() {
+  showListView();
 }
 
-// function pathClick(d, index) {
-//   visPaths.each(function (d, i) {
-//     if (i === index) {
-//       d3.select(this).classed("active", true);
-//       d3.select(this).classed("muted", false);
-//     } else {
-//       d3.select(this)
-//         .classed("muted", true)
-//         .transition()
-//         .duration(downTime)
-//         .attr("d", pathDown);
-//     }
-//   });
-//   //   d3.select(this).classed("active", true);
-//   mousePaths.each(function (d, i) {
-//     if (i !== index) {
-//       d3.select(this).transition().duration(downTime).attr("d", pathDown);
-//     }
-//   });
-// }
+function pathMouseOver(d, index) {
+  console.log("pathMouseOver");
+  const pathData = d;
+  overlay.classed("active", true);
+  visPaths.each(function (d, i) {
+    if (i === index) {
+      d3.select(this).classed("hover", true).classed("muted", false);
+    } else {
+      d3.select(this).classed("hover", false);
+    }
+    overlay
+      .classed("active", true)
+      .select(".title")
+      .html(pathData.item_description);
+
+    const salesString = numToCurrency.format(pathData.sales_total);
+
+    overlay.select(".number").html(salesString);
+  });
+}
+
+function pathMouseOut(d, index) {
+  visPaths.classed("hover", false);
+  overlay.classed("active", false);
+}
+
+function createLine(d) {
+  //   const x1 = 200;
+  const y1 = 400;
+  const points = [
+    [x0, y0],
+    [xCenter, svgWidth],
+    [x1, svgHeight],
+  ];
+  const path = d3.path();
+  path.moveTo(x0, y0);
+  path.quadraticCurveTo(...points[1], ...points[2]);
+  return path;
+}
+
+function pathIn(d) {
+  const points = [
+    [x0, y0],
+    [xCenter, yScale(d.sales_total)],
+    [x1, svgHeight],
+  ];
+  const path = d3.path();
+  path.moveTo(x0, y0);
+  path.quadraticCurveTo(...points[1], ...points[2]);
+  return path;
+}
+
+function pathDown(d) {
+  const points = [
+    [x0, y0],
+    [xCenter, yScale(d.sales_total) + pathYDown],
+    [x1, svgHeight],
+  ];
+  const path = d3.path();
+  path.moveTo(x0, y0);
+  path.quadraticCurveTo(...points[1], ...points[2]);
+  return path;
+}
+
+function pathOut(d) {
+  // { cpx: xCenter, cpy: 400 },
+  const points = [
+    [x0, y0],
+    [xCenter, svgHeight],
+    [x1, svgHeight],
+  ];
+  const path = d3.path();
+  path.moveTo(x0, y0);
+  path.quadraticCurveTo(...points[1], ...points[2]);
+  return path;
+}
+
+function positionOverlay(d, i) {
+  const mousePos = d3.mouse(this);
+  overlay.style("left", `${mousePos[0] + overlayXOffset}px`);
+  overlay.style("top", `${mousePos[1] - overlayYOffset}px`);
+}
 
 function transformData(data) {
   const newData = data.map((obj) => {
@@ -261,3 +341,24 @@ function getJson(callback, dataUrl) {
   };
   xobj.send(null);
 }
+
+// function pathClick(d, index) {
+//   visPaths.each(function (d, i) {
+//     if (i === index) {
+//       d3.select(this).classed("active", true);
+//       d3.select(this).classed("muted", false);
+//     } else {
+//       d3.select(this)
+//         .classed("muted", true)
+//         .transition()
+//         .duration(downTime)
+//         .attr("d", pathDown);
+//     }
+//   });
+//   //   d3.select(this).classed("active", true);
+//   mousePaths.each(function (d, i) {
+//     if (i !== index) {
+//       d3.select(this).transition().duration(downTime).attr("d", pathDown);
+//     }
+//   });
+// }
